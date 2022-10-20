ed9b63346d072090f35b738a4c65fa4ded84d71f
mbasadi <asadi.mohammadbagher@gmail.com>
2022-10-20 18:07:21 -0230

    update test

--- service/send/send_test.go
+++ service/send/send_test.go
@@ -3,7 +3,7 @@ package send
 import "testing"
 	
 func TestHelloWorld(t *testing.T) {
-	if HelloWorld() != "Hello, World" {
+	if HelloWorld() != "Hello, World2" {
 		t.Errorf("HelloWorlf = %s, want \"Hello, World\"", HelloWorld())
 	}
 }
